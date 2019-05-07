029c392ebc57030029eff21d3f4db8ef85c7c570
pandudewata <48473582+pandudewata@users.noreply.github.com>
2019-05-07 22:08:30 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=jRq1GPxieN5bb6cvFIIxQoDx0aGmHtcJzt9V4mgL";
         
         $("#loadImage").click(function(){
             $.ajax({
